8423c165e6007fe231e8efc2afb9c24ccdcbcf0a
hyordi12 <ucanalexis48@gmail.com>
2024-06-10 00:47:10 -0600

    spinner_planetas

--- app/src/main/java/com/example/sistemasolar/MainActivity.java
+++ app/src/main/java/com/example/sistemasolar/MainActivity.java
@@ -1,6 +1,4 @@
 package com.example.sistemasolar;
-
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -12,36 +10,50 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private List<String> nombresPlanetas = Arrays.asList(
-            "Mercúrio", "Vênus", "Tierra", "Marte",
-            "Júpiter", "Saturno", "Urano", "Neptuno"
-    );
-    private List<Integer> idsImagens = Arrays.asList(
-            R.drawable.mercurio, R.drawable.venus,
-            R.drawable.tierra, R.drawable.marte,
-            R.drawable.jupiter, R.drawable.saturno,
-            R.drawable.urano, R.drawable.neptuno
-    );
-    private List<String> descripcionesPlanetas = Arrays.asList(
-            "Mercúrio es el planeta más cercano al Sol.",
-            "Vênus es el segundo planeta más cercano al Sol y el más caliente.",
-            "Tierra es el tercer planeta desde el Sol y el único conocido que tiene vida.",
-            "Marte es el cuarto planeta desde el Sol y el más rojo.",
-            "Júpiter es el quinto planeta desde el Sol y el más grande.",
-            "Saturno es el sexto planeta desde el Sol y el más grande después de Júpiter.",
-            "Urano es el séptimo planeta desde el Sol y el más frío.",
-            "Neptuno es el octavo y último planeta desde el Sol."
-    );
+    private List<String> nombresPlanetas;
+    private List<Integer> idsImagens;
+    private List<String> descripcionesPlanetas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        nombresPlanetas = new ArrayList<>();
+        nombresPlanetas.add(getString(R.string.planeta_mercurio));
+        nombresPlanetas.add(getString(R.string.planeta_venus));
+        nombresPlanetas.add(getString(R.string.planeta_tierra));
+        nombresPlanetas.add(getString(R.string.planeta_marte));
+        nombresPlanetas.add(getString(R.string.planeta_jupiter));
+        nombresPlanetas.add(getString(R.string.planeta_saturno));
+        nombresPlanetas.add(getString(R.string.planeta_urano));
+        nombresPlanetas.add(getString(R.string.planeta_neptuno));
+
+
+        descripcionesPlanetas = new ArrayList<>();
+        descripcionesPlanetas.add(getString(R.string.descripcion_planeta_mercurio));
+        descripcionesPlanetas.add(getString(R.string.descripcion_planeta_venus));
+        descripcionesPlanetas.add(getString(R.string.descripcion_planeta_tierra));
+        descripcionesPlanetas.add(getString(R.string.descripcion_planeta_marte));
+        descripcionesPlanetas.add(getString(R.string.descripcion_planeta_jupiter));
+        descripcionesPlanetas.add(getString(R.string.descripcion_planeta_saturno));
+        descripcionesPlanetas.add(getString(R.string.descripcion_planeta_urano));
+        descripcionesPlanetas.add(getString(R.string.descripcion_planeta_neptuno));
+
+
+        idsImagens = Arrays.asList(
+                R.drawable.mercurio, R.drawable.venus,
+                R.drawable.tierra, R.drawable.marte,
+                R.drawable.jupiter, R.drawable.saturno,
+                R.drawable.urano, R.drawable.neptuno
+        );
 
         Spinner spinnerPlanetas = findViewById(R.id.spinnerPlanetas);
 
@@ -52,7 +64,6 @@ public class MainActivity extends AppCompatActivity {
 
         LinearLayout vistaPersonalizada = findViewById(R.id.vistaPersonalizada);
         vistaPersonalizada.setVisibility(View.GONE);
-
 
         spinnerPlanetas.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -70,12 +81,8 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
                 vistaPersonalizada.setVisibility(View.GONE);
             }
         });
     }
 }
-
-
-
